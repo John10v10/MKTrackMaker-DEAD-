a57448d34a517d287515cd1e7391de0e2a83252b
Jonathan Chole <Jonathan Chole>
2017-07-28 19:16:37 -0500

    Add project files.

---
+++ MarioKartTrackMaker/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace MarioKartTrackMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewPortPanel1 = new MarioKartTrackMaker.ViewPortPanel();
+            this.SuspendLayout();
+            // 
+            // viewPortPanel1
+            // 
+            this.viewPortPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.viewPortPanel1.BackColor = System.Drawing.Color.Black;
+            this.viewPortPanel1.Location = new System.Drawing.Point(12, 102);
+            this.viewPortPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.viewPortPanel1.Name = "viewPortPanel1";
+            this.viewPortPanel1.Size = new System.Drawing.Size(1165, 624);
+            this.viewPortPanel1.TabIndex = 0;
+            this.viewPortPanel1.VSync = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 741);
+            this.Controls.Add(this.viewPortPanel1);
+            this.Name = "Form1";
+            this.Text = "Mario Kart Track Maker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        
+        private ViewPortPanel viewPortPanel1;
+    }
+}
+
---
+++ MarioKartTrackMaker/ViewPortPanel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+namespace MarioKartTrackMaker
+{
+    public partial class ViewPortPanel : GLControl
+    {
+        public ViewPortPanel()
+        {
+            InitializeComponent();
+        }
+        bool loaded = false;
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (!DesignMode)
+            {
+                GL.ClearColor(Color.Black);
+                loaded = true;
+            }
+        }
+        int mx, my = 0;
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (!DesignMode)
+            {
+                Console.WriteLine("...");
+                Matrix4 mtx = Matrix4.CreateOrthographic(Width, Height, 0.01F, 10F);
+                GL.LoadMatrix(ref mtx);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+                GL.Clear(ClearBufferMask.DepthBufferBit);
+                GL.Begin(PrimitiveType.Polygon);
+                GL.Color3(Color.Red);
+                for (int i = 0; i < 360; i += 60)
+                {
+                    GL.Vertex3(Math.Sin(i * Math.PI / 180) * 30 + mx - Width / 2, Math.Cos(i * Math.PI / 180) * 30 - my + Height / 2, -5);
+                }
+                GL.End();
+                GL.Begin(PrimitiveType.Polygon);
+                GL.Color3(Color.White);
+                for (int i = 0; i < 360; i += 60)
+                {
+                    GL.Vertex3(Math.Sin(i * Math.PI / 180) * 20 + mx - Width / 2, Math.Cos(i * Math.PI / 180) * 20 - my + Height / 2, -5);
+                }
+                GL.End();
+                SwapBuffers();
+            }
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            mx = e.X;
+            my = e.Y;
+            Invalidate();
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (!DesignMode && loaded)
+            {
+                GL.Viewport(0, 0, Width, Height); 
+            }
+            Invalidate();
+        }
+    }
+}
